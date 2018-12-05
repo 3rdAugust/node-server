a6951f06c0d785fe505dd993eacc577279d6b929
Huy Duy <duy.sharp1998@gmail.com>
2018-12-05 18:45:17 +0700

    fix port method

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const express = require('express');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use((req, res, next) => {
